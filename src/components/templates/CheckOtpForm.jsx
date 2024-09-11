fdc2d40a2686ca74a95550377edf348071354888
Maryam <tavalla1991@gmail.com>
2024-09-11 08:18:22 -0700

    form2

--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-function CheckOtpForm() {
+function CheckOtpForm({code,setCode,mobile,setStep}) {
+  const submitHandler=(event)=>{
+    event.preventDefault();
+    console.log({code,mobile})
+  }
   return (
-    <div>CheckOtpForm</div>
+   <form onSubmit={submitHandler}>
+    <p>تایید کد پیامک شده</p>
+    <span>کد پیامک شده به  شماره {mobile} را وارد کنید.</span>
+    <label htmlFor='input'>کد تایید را وارد کنید.</label>
+    <input type="text" placeholder='کد تایید' id="input" value={code} onChange={e=>setCode(e.target.value)} />
+    <button type="submit">ورود</button>
+    <button onClick={()=>setStep(1)}>تغییر شماره موبایل</button>
+   </form>
   )
 }
 
